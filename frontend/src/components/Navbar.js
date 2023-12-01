ac9f69b1f314b2e8cdae0e8a36fb9a39fec12590
Pedro Ribeiro <prabadanribeiro@gmail.com>
2023-12-01 14:04:36 -0500

    routing-v1

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,21 +1,26 @@
 import React from 'react'
-import Link from 'react-router-dom'
+import { Link } from 'react-router-dom'
 import '../styles/Navbar.css'
 
-const Navbar = (props) => {
+export default function Navbar(props) {
     const background = props.home
     return (
         <div className="background">
             <nav className="navbar">
             <a className="logo">Your Logo</a>
             <ul className="nav-items">
-                <li><a className="nav-item">Home</a></li>
-                <li><a className="nav-item">About</a></li>
-                <li><a className="nav-item">Topics</a></li>
+                <li>
+                    <a className="nav-item">Home</a>
+                </li>
+                <li>
+                    <a className="nav-item">About</a>
+                </li>
+                <li>
+                    <a className="nav-item">Topics</a>
+                </li>
             </ul>
             </nav>
         </div>
     )
 }
 
-export default Navbar
