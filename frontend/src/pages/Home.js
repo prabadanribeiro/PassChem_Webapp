4af08bf2657cf7a1d39f513e2e0766ed6c55aa7e
logaham <152742404+logaham@users.noreply.github.com>
2023-12-04 18:29:38 -0500

    Added Divider.js in components folder and Divider.css in styles folder.
    Purely for looks.

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import Navbar from '../components/Navbar'
+import Divider from '../components/Divider'
 
 export default function Home() {
     return (
         <div>
             <Navbar home={true}/>
+            <Divider/>
         </div>
     )
 }
