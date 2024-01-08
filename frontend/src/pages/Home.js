55704e7a0d16ba6d8f6779af2b39b2f7c3dc231c
logaham <152742404+logaham@users.noreply.github.com>
2024-01-07 19:07:20 -0500

    added the start of a quotes component and changed some small things

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import Navbar from '../components/Navbar'
 import Main from '../components/Main'
+import Quotes from '../components/Quotes'
 
 export default function Home() {
     return (
         <div>
             <Navbar isHome/>
             <Main/>
+            <Quotes/>
         </div>
     )
 }
