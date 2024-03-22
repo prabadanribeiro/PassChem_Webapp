4ce354b1f7c1449471ba5d1fb7f6cb0affdecf37
logaham <152742404+logaham@users.noreply.github.com>
2024-03-22 19:14:06 -0400

    redesigned topics + added socials to footer

--- frontend/src/components/TopicsGrid.js
+++ frontend/src/components/TopicsGrid.js
@@ -20,20 +20,27 @@ export default function Topics () {
 
     return (
         <div className='topics-body'>
-            <h2 className='topics-header'>Topics Collection</h2>
+            <h3>AP Chem Curriculum</h3>
+            <hr></hr>
             <div className='topics-container'>
                 { // use topics.img
                     topics.map(topic =>
-                        <l1 key={topic.unit} className="topics-cards">
-                            <Link to={`/topics/${encodeURIComponent(topic.title)}`} className='card-content'>
-                                <h4 className='cards-header'>
+                        <Link to={`/topics/${encodeURIComponent(topic.title)}`} className='link'>
+                            <div className='topics-cards'>
+                                <img className="topics-images" src={`http://127.0.0.1:8000/${topic.img}`} alt='Unit Images'/>
+                                <div className='topics-description'>
+                                    <p>{/* needs topic.description */}Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+                                </div>
+                            </div>
+                            <l1 key={topic.unit} className="card-content">
+                                <h4>
                                     Unit {topic.unit}:  
                                 </h4>
-                                <h4 className='cards-unit'>
+                                <h4>
                                     &nbsp;{topic.title} 
                                 </h4>
-                            </Link>
-                        </l1>
+                            </l1>
+                        </Link>
                     )
                 }
             </div>
