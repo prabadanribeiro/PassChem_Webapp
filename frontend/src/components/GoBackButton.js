b4761bf98af626ebef8bacdfef18a1010b997d55
Pedro Ribeiro <prabadanribeiro@gmail.com>
2024-06-30 16:51:49 -0400

    fixed weird bugs

--- frontend/src/components/GoBackButton.js
+++ frontend/src/components/GoBackButton.js
@@ -6,12 +6,6 @@ function GoBackButton({ page, topicTitle, unitTitle }) {
 
     let buttonType = null
 
-<<<<<<< HEAD
-    if (page === "Lesson Page") {
-        buttonType = (
-            <div>
-                <Link to={`/curriculum/${URLService.slugify(unitTitle)}/${URLService.slugify(topicTitle)}`}>Go Back to Topic</Link>
-=======
     const backButton = {
         position: 'absolute',
         top: '108px',
@@ -21,27 +15,22 @@ function GoBackButton({ page, topicTitle, unitTitle }) {
         borderBottom: '3px solid #111',
     }
 
-    if (page === "Lesson") {
+    if (page === "Lesson Page") {
         buttonType = (
             <div>
-                <Link to={`/topics/${encodeURIComponent(topicTitle)}`} style={backButton}><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 -960 960 960" fill="#111111"><path d="M400-240 160-480l240-240 56 58-142 142h486v80H314l142 142-56 58Z"/></svg></Link>
->>>>>>> master
+                <Link to={`/curriculum/${URLService.slugify(unitTitle)}/${URLService.slugify(topicTitle)}`} style={backButton}><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 -960 960 960" fill="#111111"><path d="M400-240 160-480l240-240 56 58-142 142h486v80H314l142 142-56 58Z"/></svg></Link>
             </div>
         )
     } else if (page === "Topic Page") {
         buttonType = (
             <div>
-<<<<<<< HEAD
-                <Link to={`/curriculum/${URLService.slugify(unitTitle)}`}>Go Back to Unit</Link>
+                <Link to={`/curriculum/${URLService.slugify(unitTitle)}`} style={backButton}><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 -960 960 960" fill="#111111"><path d="M400-240 160-480l240-240 56 58-142 142h486v80H314l142 142-56 58Z"/></svg></Link>
             </div>
         )
     } else if (page === "Unit Page") {
         buttonType = (
             <div>
-                <Link to={'/curriculum'}>Go Back to Curriculum</Link>
-=======
-                <Link to={'/topics/'} style={backButton}><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 -960 960 960" fill="#111111"><path d="M400-240 160-480l240-240 56 58-142 142h486v80H314l142 142-56 58Z"/></svg></Link>
->>>>>>> master
+                <Link to={'/curriculum'} style={backButton}>Go Back to Curriculum</Link>
             </div>
         )
     }
