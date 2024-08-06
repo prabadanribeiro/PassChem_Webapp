e580a6d3c5703d2284fa9116cf214faa9fde9aab
Pedro Ribeiro <prabadanribeiro@gmail.com>
2024-08-06 13:10:05 -0400

    added description but nothing in the database for now

--- frontend/src/services/AxiosServices.js
+++ frontend/src/services/AxiosServices.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 import Cookies from 'js-cookie'
 
-const api = axios.create({ // CHANGE ALL INSTANCES WHERE AXIOS IS BEING USED TO USE API AS IT WILL MAKE THE INTECEPTOR RUN EVERY TIME
+const api = axios.create({
     baseURL: 'http://127.0.0.1:8000/'
 })
 
