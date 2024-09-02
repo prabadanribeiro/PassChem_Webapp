d3734dd0d9764ca02111be6bb97b12afac65d88a
logaham <152742404+logaham@users.noreply.github.com>
2024-09-01 21:50:08 -0400

    sign-up page v1

--- frontend/src/components/SignupForm.js
+++ frontend/src/components/SignupForm.js
@@ -4,6 +4,7 @@ import { useNavigate } from 'react-router-dom'
 import axios from 'axios'
 import config from '../config/Config'
 import api from '../services/AxiosServices'
+import '../styles/SignUp.css'
 
 function loadGoogleScript() {
     
@@ -70,7 +71,7 @@ export default function SignupForm() {
             
             google.accounts.id.renderButton(
                 document.getElementById('GoogleSignIn'),
-                { theme: 'outline', size: 'large' }
+                { theme: 'outline', size: 'large', type: 'icon' }
             )
         })
     }, [])
@@ -120,11 +121,11 @@ export default function SignupForm() {
     }
 
     return (
-        <div>
+        <div className='sign-up-hero'>
             <form onSubmit={submit}>
-                <div>
-                    <h3>Register</h3>
-                    <div>
+                <div className='sign-up-content'>
+                    <h3>Register an account</h3>
+                    <div className='sign-up-input'>
                         <label>First Name</label>
                         <input
                             placeholder="First Name" 
@@ -135,7 +136,7 @@ export default function SignupForm() {
                             onChange={e => setFirstName(e.target.value)}
                         />
                     </div>
-                    <div>
+                    <div className='sign-up-input'>
                         <label>Last Name</label>
                         <input
                             placeholder="Last Name" 
@@ -146,7 +147,7 @@ export default function SignupForm() {
                             onChange={e => setLastName(e.target.value)}
                         />
                     </div>
-                    <div>
+                    <div className='sign-up-input'>
                         <label>Email</label>
                         <input
                             placeholder="Email" 
@@ -157,7 +158,7 @@ export default function SignupForm() {
                             onChange={e => setEmail(e.target.value)}
                         />
                     </div>
-                    <div>
+                    <div className='sign-up-input'>
                         <label>Password</label>
                         <input 
                             name='password' 
@@ -168,12 +169,12 @@ export default function SignupForm() {
                             onChange={e => setPassword(e.target.value)}
                         />
                     </div>
-                    <div>
+                    <div className='sign-up-buttons-container'>
                         <button type="submit">Register</button>
+                        <div id='GoogleSignIn' className='google-sign-up' data-type='icon'></div>
                     </div>
                 </div>
             </form>
-            <div id='GoogleSignIn'></div>
        </div>
     )
 }
