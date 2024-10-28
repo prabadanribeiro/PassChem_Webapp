49a1f2d277ae501ea5f6989d15842c15a8c19568
logaham <152742404+logaham@users.noreply.github.com>
2024-10-28 01:07:51 -0400

    misc

--- frontend/src/components/SignupForm.js
+++ frontend/src/components/SignupForm.js
@@ -180,6 +180,7 @@ export default function SignupForm() {
                         />
                     </div>
                     {errorMessage && <div>{errorMessage}</div>}
+                    <p>*Sponholtz Productions will not sell or distrbute your information</p>
                     <div className='sign-up-buttons-container'>
                         <button type="submit">Register</button>
                         <div id='GoogleSignIn' className='google-sign-up' data-type='icon'></div>
